3fc224dbc0504c463f1b3864fc99b7a3119cd041
snaucorp <saidur.rahman@springrain.io>
2022-08-12 09:27:16 +0600

    redux setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
 import "./styles/tailwind.css";
 import "./index.css";
 
@@ -10,7 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,43 +1,22 @@
 import React, { useEffect } from "react";
-import { useState } from "react";
+import Counter from "./Counter";
+import Tick from "./Tick";
 import Button from "../components/Button";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-  const [click, setClick] = useState(false);
-
   useEffect(() => {
-    if (count === 5) {
-      console.log("In the count");
-    }
     console.log("Last Step");
     return () => {
       console.log("cleanUp");
     };
-  }, [count]);
-
+  }, []);
 
   return (
-    <div className="p-2">
-      <div className="grid grid-cols-2 py-2 justify-items-center gap-4 ">
-        <div>Click Here </div>
-        <div>
-          <Button className="bg-green-600" onClick={() => setCount(count + 1)}>
-            Click Me
-          </Button>
-        </div>
-        <div>You clicked for </div>
-        <div>{count} times</div>
-      </div>
-      <div className="flex justify-center">
-        <Button
-          className={`bg-${click ? "green" : "pink"}-600`}
-          onClick={() => setClick(!click)}
-        >
-          {click ? "tick" : "untick"}{" "}
-        </Button>
-      </div>
-    </div>
+    <>
+      <Counter />
+      <Tick />
+      <div>Changes happend</div>
+    </>
   );
 };
 
